e602e320f642c1e7c41918c3eb9d600e8c529433
Renan <rvmoura.96@gmail.com>
2019-09-27 23:08:48 -0300

    Ignoring testfiles on flake8, PEP-257 fix.

--- load_balancer.py
+++ load_balancer.py
@@ -1,25 +1,4 @@
 """
-Problema
-
-Balanceamento de carga é muito importante em ambientes Cloud. Estamos sempre tentando minimizar os custos para que
-possamos manter o número de servidores o menor possível. Em contrapartida a capacidade e performance aumenta quando
-adicionamos mais servidores.
-
-Em nosso ambiente de simulação, em cada tick (unidade básica de tempo da simulação), os usuários conectam aos
-servidores disponíveis e executam uma tarefa. Cada tarefa leva um número de ticks para ser finalizada (o número de
-ticks de uma tarefa é representado por ttask ), e após isso o usuário se desconecta automaticamente.
-Os servidores são máquinas virtuais que se auto criam para acomodar novos usuários. Cada servidor custa R$ 1,00 por
-tick e suporta no máximo umax usuários simultaneamente. Você deve finalizar servidores que não estão sendo mais usados.
-O desafio é fazer um programa em Python que recebe usuários e os aloca nos servidores tentando manter o menor custo
-possível.
-
-Input
-Um arquivo onde:
-a primeira linha possui o valor de ttask ;
-a segunda linha possui o valor de umax ;
-as demais linhas contém o número de novos usuários para cada tick .
-
-
 Constant value for tick cost COST_PER_TICK = 1.00
 tick -> basic time unit
 ttask -> ticks for a task conclusion
