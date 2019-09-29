cd2b5b7133737d896e42eb930a4945fd0506a89b
Renan <rvmoura.96@gmail.com>
2019-09-29 15:59:24 -0300

    Adding docstrings for loadbalancer functions.

--- load_balancer.py
+++ load_balancer.py
@@ -11,6 +11,7 @@ class LoadBalancer:
         self.total_users = int()
 
     def calculate_user(self) -> int:
+        """Count the total of users between all servers."""
         total_users = int()
 
         for server in self.servers:
@@ -31,6 +32,7 @@ class LoadBalancer:
         Assign a task to an existent server or create a
         new server and assign the task to the new instance.
         """
+
         for i in range(users):
             for server in self.servers:
                 if server.available():
@@ -42,10 +44,24 @@ class LoadBalancer:
                 self.servers.append(server)
 
     def run_tasks(self):
+        """Execute tasks in all servers.
+
+        Execute all tasks in all servers, reducing the ttask by one,
+        also sum the count of servers to represent how much was the cost
+        for each tick run. After execute tasks, if some of the servers are
+        empty it will be removed from servers attribute.
+        """
+
         for server in self.servers:
             server.execute_tasks()
         self.total += len(self.servers)
         self.servers = [s for s in self.servers if not s.is_empty()]
 
     def output(self):
+        """"Just an output message.
+
+        An output representation composed by length of users in each server,
+        separated by comma.
+        """
+
         return ", ".join(str(len(s.users)) for s in self.servers)
